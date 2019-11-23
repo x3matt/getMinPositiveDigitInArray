e2a1dd4ebe200fc14952e83b469d4d2f2d12176a
Vladislav <x3matt21@mail.ru>
2019-11-23 20:27:12 +0300

    edited code

--- src/Controller.java
+++ src/Controller.java
@@ -4,6 +4,11 @@ public class Controller {
     int getMinPositiveDigitInArray(int[] array){
         int[] positiveDigits = Arrays.stream(array).filter(i -> i > 0).toArray();
         Arrays.sort(positiveDigits);
-        return positiveDigits[0];
+        int digit = 1;
+        for (int num : positiveDigits) {
+            if (digit == num) {digit++;}
+            else break;
+        }
+        return digit;
     }
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -15,12 +15,12 @@ public class TestClass {
     @Test
     public void testGetMinPositiveDigitInArray_valid(){
         int[] array1 = {-1,-5,-7,1,6,-2,5,2,7,0};
-        assertEquals(1,controller.getMinPositiveDigitInArray(array1));
+        assertEquals(3,controller.getMinPositiveDigitInArray(array1));
         int[] array2 = {-7,10,-2,-8,-7,2,19,111};
-        assertEquals(2,controller.getMinPositiveDigitInArray(array2));
+        assertEquals(1,controller.getMinPositiveDigitInArray(array2));
         int[] array3 = {15,88,-150,-11,56,87,-2501};
-        assertEquals(15,controller.getMinPositiveDigitInArray(array3));
-        int[] array4 = {-6,4,7,-2,-66,-12,-54,54};
-        assertEquals(4,controller.getMinPositiveDigitInArray(array4));
+        assertEquals(1,controller.getMinPositiveDigitInArray(array3));
+        int[] array4 = {-6,4,7,-2,1,-66,-12,-54,54};
+        assertEquals(2,controller.getMinPositiveDigitInArray(array4));
     }
 }
